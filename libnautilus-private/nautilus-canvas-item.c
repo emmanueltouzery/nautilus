c536fa3676aad2380a6b0217ad1fb92f24232e26
Emmanuel Touzery <etouzery@gmail.com>
2014-10-06 21:11:56 +0200

    now the selection checkbox reflects the actual selection status

--- libnautilus-private/nautilus-canvas-item.c
+++ libnautilus-private/nautilus-canvas-item.c
@@ -1262,6 +1262,7 @@ nautilus_canvas_item_draw (EelCanvasItem *item,
 {
 	NautilusCanvasContainer *container;
 	NautilusCanvasItem *canvas_item;
+	NautilusCanvasIcon *canvas_icon;
 	NautilusCanvasItemDetails *details;
 	EelIRect icon_rect;
 	cairo_surface_t *temp_surface;
@@ -1298,7 +1299,10 @@ nautilus_canvas_item_draw (EelCanvasItem *item,
 		container = NAUTILUS_CANVAS_CONTAINER (EEL_CANVAS_ITEM (item)->canvas);
 		gtk_style_context_save (context);
 		gtk_style_context_add_class (context, GTK_STYLE_CLASS_CHECK);
-		gtk_style_context_set_state (context, GTK_STATE_FLAG_CHECKED);
+		canvas_icon = canvas_item->user_data;
+		if (canvas_icon->is_selected) {
+			gtk_style_context_set_state (context, GTK_STATE_FLAG_CHECKED);
+		}
 		gtk_render_check(context, cr, icon_rect.x0, icon_rect.y0, 16, 16);
 		gtk_style_context_restore (context);
 	}
